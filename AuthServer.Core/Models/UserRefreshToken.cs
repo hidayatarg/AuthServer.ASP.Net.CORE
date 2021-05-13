b7599caa422d32942ad8458535672dfa435110bf
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-05-13 12:18:40 +0300

    Added Models - Core

---
+++ AuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
