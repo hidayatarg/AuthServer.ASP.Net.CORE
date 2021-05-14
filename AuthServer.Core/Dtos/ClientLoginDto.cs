577d10f208b2918f3ef9f555998642a9aa2279bd
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-05-14 15:04:54 +0300

    Implemented IAuthenticationService Interface - Core

---
+++ AuthServer.Core/Dtos/ClientLoginDto.cs
@@ -0,0 +1,9 @@
+namespace AuthServer.Core.Dtos
+{
+    public class ClientLoginDto
+    {
+        public string ClientId { get; set; }
+        
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
