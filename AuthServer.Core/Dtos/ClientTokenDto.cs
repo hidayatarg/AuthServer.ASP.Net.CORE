a71d3762e9423513e08f793fc308e2d1cb30c789
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-05-14 14:42:50 +0300

    Implemented TokenDto classes  - Core

---
+++ AuthServer.Core/Dtos/ClientTokenDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthServer.Core.Dtos
+{
+    public class ClientTokenDto
+    {
+        public string AccessToken { get; set; }
+        
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
