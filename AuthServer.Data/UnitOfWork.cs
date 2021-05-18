446ca557f2e91d6b5554e3138704c932d2502270
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-05-18 03:14:10 +0300

    Implemented UnitOfWork  Class - Data

---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
