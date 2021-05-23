07133c26a2156f43bb787ce4a31424fc7aaa5552
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-05-23 20:02:37 +0300

    Implemented TokenService GetClaim - Service

--- AuthServer.Service/Services/TokenServices.cs
+++ AuthServer.Service/Services/TokenServices.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using AuthServer.Core.Configuration;
 using AuthServer.Core.Dtos;
@@ -6,6 +10,7 @@ using AuthServer.Core.Models;
 using AuthServer.Core.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
 using SharedLibrary.Configurations;
 
 namespace AuthServer.Service.Services
@@ -31,6 +36,23 @@ namespace AuthServer.Service.Services
             random.GetBytes(numberBytes);
 
             return Convert.ToBase64String(numberBytes);
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp userApp, List<string> audience)
+        {
+            // all data to add in the payload as claim
+            var userList = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, userApp.Id),
+                // new Claim("email", userApp.Email) same
+                new Claim(JwtRegisteredClaimNames.Email, userApp.Email),
+                new Claim(ClaimTypes.Name, userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            
+            userList.AddRange(audience.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+
+            return userList;
         }
         
         public TokenDto CreateToken(UserApp userApp)
